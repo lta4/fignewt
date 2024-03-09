1bc81c3779539721e17611fe05a20b0bb3252dd6
Andrew <andrew.mk4@yahoo.com>
2024-03-09 04:04:46 -0800

    index router and main page added

---
+++ src/pages/main.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const Main = (props) => {
+    return (
+        <div>
+            <p>This is the main feed</p>
+        </div>
+    );
+}
+
+export default Main;
