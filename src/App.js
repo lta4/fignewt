c1be30ba81970e0b19011be536aa57ec4447ea6f
Andrew <andrew.mk4@yahoo.com>
2024-03-09 03:42:13 -0800

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+// import logo from './logo.svg';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) =>res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        Hi Newt
+        Hi FigNewt
       </header>
     </div>
   );
