9553f953f386f5827baa1c6b40f30610f237b6ea
Andrew <andrew.mk4@yahoo.com>
2024-03-20 14:54:42 -0700

    nav layout addition

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,15 +3,26 @@ import { NavLink } from "react-router-dom";
 
 const Nav = (props) => {
     return (
-        <div className="navBar">
-            <NavLink to="/">
-                <h1 className="title">FigNewt</h1>
-            </NavLink>
-            <NavLink to="/fav">Fav</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/review">Review</NavLink>
-            {/* <NavLink to="/main">Main</NavLink> */}
-        </div>
+        <header className="header">
+            <nav className="navBar">
+                <NavLink to="/" className="nav__logo">FigNewt
+                </NavLink>
+                <div className={"nav__menu"} id="nav-menu">
+                    <ul className="nav__list">
+                        <li className="nav__item"><NavLink to="/" className="nav__link">
+                            Home
+                        </NavLink></li>
+                        <li className="nav__item"><NavLink to="/fav" className="nav__link">
+                            Fav
+                        </NavLink></li>
+                    </ul>
+                    {/* <NavLink to="/fav">Fav</NavLink> */}
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/review">Review</NavLink>
+                    {/* <NavLink to="/main">Main</NavLink> */}
+                </div>
+            </nav>
+        </header>
     );
 };
 
