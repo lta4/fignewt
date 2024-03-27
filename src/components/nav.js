fc81be8d4240d29d196d0faa82d294222c8e943e
Andrew <andrew.mk4@yahoo.com>
2024-03-27 14:48:46 -0700

    login adjusted

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { IoClose, IoMenu } from "react-icons/io5";
+import "./nav.css";
 
 const Nav = (props) => {
     return (
         <header className="header">
-            <nav className="navBar">
-                <NavLink to="/" className="nav__logo">FigNewt
+            <nav className="nav container">
+                <NavLink to="/" className="nav__logo">
+                    Navigation Bar
                 </NavLink>
                 <div className={"nav__menu"} id="nav-menu">
                     <ul className="nav__list">
@@ -19,9 +21,14 @@ const Nav = (props) => {
                         <li className="nav__item"><NavLink to="/about" className="nav__link">
                             About
                         </NavLink></li>
-                        <li className="na__item"><NavLink to="/review" className="nav__link">
+                        <li className="nav__item"><NavLink to="/review" className="nav__link">
                             Review
                         </NavLink>
+                        </li>
+                        <li className="nav__item">
+                            <NavLink to="/get-started" className="nav__link nav__cta">
+                                Get Started
+                            </NavLink>
                         </li>
                     </ul>
                     <div className="nav__close" id="nav-close">
