2cf7210dd06aa99ab1ed145d34bfb16f9f9c17a1
Andrew <andrew.mk4@yahoo.com>
2024-04-05 01:05:59 -0700

    about profile set up

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,22 @@ import React from "react";
 const About = (props) => {
     return (
         <>
+        <div className="profileTitle" id="b">
             <h1>About DJ K Fig</h1>
-                <div>
-                    <h2>The content of K and Fig</h2>
-                    <p>Lorem Opios</p>
+        </div>
+        <section className="profile">
+            <div className="profile__set">
+                <div className="profile__set__image">
+                    <img src="https://res.cloudinary.com/dfltkqotn/image/upload/v1636925630/Profile-Image.jpg" alt="profile of one's self" height="350px" />
+                        <div className="profile__set__description">
+                            <h2>I am DJ K Fig</h2>
+                            <p>I'm a Full Stack Developer residing in San Diego, CA.
+                            I have a serious passion for developing, animations, and creating intuitive/dynamic user experiences.</p>
+                            <p>Let's create something special!</p>
+                        </div>
                 </div>
+            </div>
+        </section>
         </>
     );
 };
