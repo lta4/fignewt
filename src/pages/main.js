882338188b18c7a06364ec9c34069c19ae844893
Andrew <andrew.mk4@yahoo.com>
2024-04-17 03:28:24 -0700

    page divider all around

--- src/pages/main.js
+++ src/pages/main.js
@@ -5,14 +5,14 @@ import ContactForm from "../components/contactForm";
 const Main = (props) => {
     return (
         <>
-        <header></header>
-            <div className="page-divider">
+        <header>
+            <div className="page-divider"></div>
+        </header>
             <div style={{ backgroundImage: `url("https://res.cloudinary.com/dfltkqotn/image/upload/v1636925630/Profile-Image.jpg")` }}>
                 <h1>DJ K Fig</h1>
                 <p>DJ K Fig in the house. We are all about the hard bass and fury electronics.</p>
                 <Form />
                 <ContactForm />
-            </div>
             </div>
         </>
     );
