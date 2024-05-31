36c7ecc4cbf5bbf2a02e2c320031b2e488eeddbe
Andrew <andrew.mk4@yahoo.com>
2024-05-31 09:39:54 -0700

    picture adds width and height

--- src/pages/shows.js
+++ src/pages/shows.js
@@ -6,6 +6,12 @@ const Shows = (props) => {
         <header>
             {/* <div className="page-divider">
             </div> */}
+            <div id="dj_j">
+                <div className="page_image">
+                    <div className="image_wrapper"> <img data-src="https://res.cloudinary.com/dfltkqotn/image/upload/v1636925630/Profile-Image.jpg" data-img="https://res.cloudinary.com/dfltkqotn/image/upload/v1636925630/Profile-Image.jpg" data-image-dimensions="2500x1667" alt="" data-image-focal-point="0.5,0.5" data-image-resolution="2500w" src="https://res.cloudinary.com/dfltkqotn/image/upload/v1636925630/Profile-Image.jpg"></img>
+                    </div>
+                </div>
+            </div>
         </header>
         <section>
             <h1>hello section show</h1>
